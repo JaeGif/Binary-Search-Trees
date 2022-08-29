6129ae6552bcd1d7b5e80da810ddc2a8409cf986
Jae Gifford <jae.giff@outlook.com>
2022-08-29 12:39:50 +0800

    add package.json, restructure project to appropriate modules

---
+++ treeMethods.js
@@ -0,0 +1,29 @@
+import { prettyPrint, removeDuplicateNodes } from './utilities.js';
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+  buildTree(nodeArr) {}
+}
+
+/* 1: Initialize start = 0, end = length of the array – 1
+2: mid = (start+end)/2
+3: Create a tree node with mid as root (lets call it A).
+4: Recursively do following steps:
+5: Calculate mid of left subarray and make it root of left subtree of A.
+6: Calculate mid of right subarray and make it root of right subtree of A. */
+
+console.log(
+  removeDuplicateNodes([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+);
+
+let treeInstance = new Tree();
--- utilities.js
+++ utilities.js
@@ -1,25 +1,3 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
-  buildTree(nodeArr) {}
-}
-
-/* 1: Initialize start = 0, end = length of the array – 1
-2: mid = (start+end)/2
-3: Create a tree node with mid as root (lets call it A).
-4: Recursively do following steps:
-5: Calculate mid of left subarray and make it root of left subtree of A.
-6: Calculate mid of right subarray and make it root of right subtree of A. */
-
 function binarySearch(arr, val) {
   let start = 0;
   let end = arr.length - 1;
@@ -42,7 +20,7 @@ function binarySearch(arr, val) {
 function removeDuplicateNodes(nodeArr) {
   let nodeSet = [...new Set(nodeArr)];
 
-  console.log(nodeSet);
+  return nodeSet;
 }
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   // visualize the binary tree
@@ -55,6 +33,4 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 };
 
-removeDuplicateNodes([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
-let treeInstance = new Tree();
+export { removeDuplicateNodes, prettyPrint };
