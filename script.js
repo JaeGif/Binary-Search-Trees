3dc93f5905a484328700c486935a2fbd37ebcab8
Jae Gifford <jae.giff@outlook.com>
2022-08-29 08:53:59 +0800

    binary tree initialize add: prettyprint(), binarySearch()

---
+++ script.js
@@ -0,0 +1,30 @@
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === val) {
+      return mid;
+    }
+
+    if (val < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  // visualize the binary tree
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
